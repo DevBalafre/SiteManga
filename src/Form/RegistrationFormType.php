04e108391705dd323bf921ee2f7e263a2abce518
Fabrice mohamed <mohamedfabrice.pro@gmail.com>
2022-09-12 19:16:03 +0200

    Inscription et connexion User + implementation roles (finis)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,12 +19,33 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class,[
+                'label_attr'=>[
+                    "class" => "form-label mt-4"
+                ],
+                'attr' =>[
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('pseudo', TextType::class,[
+                'label_attr'=>[
+                    "class" => "form-label mt-4"
+                ],
+                "attr"=>[
+                    'class' => 'form-control',
+                    'minlength' => '4',
+                    'minMessage' => 'Ton pseudo doit contenir {{ limit }} caracters',
+                    // max length allowed by Symfony for security reasons
+                    'maxlength' => '50',
+                ],
+            ])
+            
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => "Termes d'utilisations",
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Tu doit accpter nos termes d\'utilisations.',
                     ]),
                 ],
             ])
@@ -30,14 +53,19 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'label_attr'=>[
+                    "class" => "form-label mt-4"
+                ],
+                'attr' => ['autocomplete' => 'new-password',
+                'class' => 'form-control',
+            ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Vous devez entrer un mot de passe',
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'min' => 4,
+                        'minMessage' => 'Ton mot de passe doit contenir au moins {{ limit }} caracters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
