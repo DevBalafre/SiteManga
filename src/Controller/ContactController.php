2436a37e9695ce6f492618f9b236848f3e9515e7
DevBalafre <mohamedfabrice.pro@gmail.com>
2022-09-22 14:26:11 +0200

    Fonctionnalité Form contact finis

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -23,22 +23,26 @@ class ContactController extends AbstractController
         $contactForm->handleRequest($request);
 
         if ($contactForm->isSubmitted() && $contactForm->isValid()) {
-            $contact = $contactForm->getData(); //recup infos formulaire
-            $email = (new TemplatedEmail()) //prépare un email via template twigù
-                ->from(new Address($contact('email'))) //expéditeur
-                ->to(new Address('mohamedfabrice.pro@gmail.com')) //destinataire
-                ->replyTo(new Address($contact['email']))
-                ->subject($contact('sujet'))
-                ->htmlTemplate('email/contact_email.html.twig')
-                ->context([
-                    'emailAdresse' => $contact['email'],
-                    'sujet' => $contact['sujet'],
-                    'message' => $contact['message']
-                    
-                ]);
+
+            if (empty($contactForm['honeypot']->getData())) { //Filtre les bots
+
+
+                $contact = $contactForm->getData(); //recup infos formulaire
+                $email = (new TemplatedEmail()) //prépare un email via template twigù
+                    ->from(new Address($contact['email'])) //expéditeur
+                    ->to(new Address('mohamedfabrice.pro@gmail.com')) //destinataire
+                    ->replyTo(new Address($contact['email']))
+                    ->subject($contact['sujet'])
+                    ->htmlTemplate('email/contact_email.html.twig')
+                    ->context([
+                        'emailAdresse' => $contact['email'],
+                        'sujet' => $contact['sujet'],
+                        'message' => $contact['message']
+
+                    ]);
 
                 if ($contact['fichier'] !== null) {
-                    $originalFilename = pathinfo($contact['fichier']->getClientOriginalName(),PATHINFO_FILENAME); // récupération du nom original du fichier
+                    $originalFilename = pathinfo($contact['fichier']->getClientOriginalName(), PATHINFO_FILENAME); // récupération du nom original du fichier
                     $safeFilename = $slugger->slug($originalFilename); // Transformation caractère spéciaux en caratère basique
                     $newFileName = $safeFilename . '.' . $contact['fichier']->guessExtension(); //renomme fichier pour ajouter extension
                     $email->attachFromPath($contact['fichier']->getPathName(), $newFileName); // attache le fichier en piece jointe
@@ -47,9 +51,10 @@ class ContactController extends AbstractController
                 $mailer->send($email);
                 $this->addFlash('success', 'Votre message a bien été envoyer');
                 return $this->redirectToRoute('app_contact');
-            
+            } else {
+                dd('Tu es un bot');
+            }
         }
-
         return $this->render('contact/index.html.twig', [
             'contactForm' => $contactForm->createView(),
         ]);
